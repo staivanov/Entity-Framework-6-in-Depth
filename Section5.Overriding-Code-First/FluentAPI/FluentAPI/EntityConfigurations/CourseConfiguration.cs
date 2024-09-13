a9790d95e08132ae8d54c6e860c03e6edf1a69c9
Stanislav Ivanov <40460292+staivanov@users.noreply.github.com>
2024-09-13 14:21:11 +0300

    Extend OnModelCreation method - Course entity in another class with EntityTypeConfiguration inheritance. This way configuration in OnModelCreation is cleaner.

---
+++ Section5.Overriding-Code-First/FluentAPI/FluentAPI/EntityConfigurations/CourseConfiguration.cs
@@ -0,0 +1,45 @@
+﻿using DataAnnotations;
+using System.Data.Entity.ModelConfiguration;
+
+namespace FluentAPI.EntityConfigurations
+{
+    public class CourseConfiguration : EntityTypeConfiguration<Course>
+    {
+        /// <summary>
+        /// Configuration instance for Course entity.
+        /// </summary>
+        public CourseConfiguration()
+        {
+            ToTable("tbl_Course");
+
+            HasKey(c => c.Id);
+
+            Property(d => d.Description)
+            .IsRequired()
+            .HasMaxLength(2000);
+
+            Property(c => c.Name)
+            .IsRequired()
+            .HasMaxLength(50);
+
+            HasRequired(c => c.Author)
+            .WithMany(a => a.Courses)
+            .HasForeignKey(c => c.AuthorId)
+            .WillCascadeOnDelete(false);
+
+            HasRequired(c => c.Cover)
+            .WithRequiredPrincipal(c => c.Course);
+
+            HasMany(t => t.Tags)
+            .WithMany(c => c.Courses)
+            .Map(m =>
+                {
+                    m.ToTable("CourseTags");
+                    m.MapLeftKey("Course_Id");
+                    m.MapRightKey("Tag_Id");
+                }
+            );
+        }
+    }
+}
+
--- Section5.Overriding-Code-First/FluentAPI/FluentAPI/PlutoContext.cs
+++ Section5.Overriding-Code-First/FluentAPI/FluentAPI/PlutoContext.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.Entity;
+﻿using FluentAPI.EntityConfigurations;
+using System.Data.Entity;
 
 namespace DataAnnotations
 {
@@ -26,40 +27,7 @@ namespace DataAnnotations
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Course>()
-                        .Property(c => c.Name)
-                        .IsRequired()
-                        .HasMaxLength(50);
-
-            modelBuilder.Entity<Course>()
-                        .Property(d => d.Description)
-                        .IsRequired()
-                        .HasMaxLength(2000);
-
-            modelBuilder.Entity<Course>()
-                .HasRequired(c => c.Author)
-                .WithMany(a => a.Courses)
-                .HasForeignKey(c => c.AuthorId)
-                .WillCascadeOnDelete(false);
-
-            modelBuilder.Entity<Course>()
-                .HasMany(t => t.Tags)
-                .WithMany(c => c.Courses)
-                .Map(m =>
-                    {
-                        m.ToTable("CourseTags");
-                        m.MapLeftKey("Course_Id");
-                        m.MapRightKey("Tag_Id");
-                    }
-                );
-
-            modelBuilder.Entity<Course>()
-              .HasRequired(c => c.Cover)
-              .WithRequiredPrincipal(c => c.Course);
-
-
-
-            base.OnModelCreating(modelBuilder);
+            modelBuilder.Configurations.Add(new CourseConfiguration());
         }
     }
 }
