efb5a52b0791c5b7f8fff1d62405561d04c9db12
Stanislav Ivanov <40460292+staivanov@users.noreply.github.com>
2024-09-13 12:06:32 +0300

    Code was documented.

--- Section5.Overriding-Code-First/DataAnnotations/DataAnnotations/Course.cs
+++ Section5.Overriding-Code-First/DataAnnotations/DataAnnotations/Course.cs
@@ -6,30 +6,58 @@ namespace DataAnnotations
 {
     public class Course
     {
+        /// <summary>
+        /// Create a new instance of Course object with Tags.
+        /// </summary>
         public Course()
         {
             Tags = new HashSet<Tag>();
         }
 
+
+        /// <summary>
+        /// Gets or sets the id of the course. 
+        /// </summary>
         public int Id { get; set; }
 
+        /// <summary>
+        /// Gets or sets the name of the course.
+        /// </summary>
         [Required]
         [MaxLength(255)]
         public string Name { get; set; }
 
+        /// <summary>
+        /// Gets or sets the description of the course.
+        /// </summary>
         [Required]
         [MaxLength(2000)]
         public string Description { get; set; }
 
+        /// <summary>
+        /// Gets or sets the level of the course.
+        /// </summary>
         public int Level { get; set; }
 
+        /// <summary>
+        /// Gets or sets the fullprice of the course.
+        /// </summary>
         public float FullPrice { get; set; }
 
+        /// <summary>
+        /// Gets or sets the authorId of the course.
+        /// </summary>
         [ForeignKey("Author")] //Navigation property
         public int AuthorId { get; set; }
 
+        /// <summary>
+        /// Gets or sets the author of the course.
+        /// </summary>
         public virtual Author Author { get; set; }
 
+        /// <summary>
+        /// Gets or sets the tags of the course.
+        /// </summary>
         public virtual ICollection<Tag> Tags { get; set; }
     }
 }
--- Section5.Overriding-Code-First/DataAnnotations/DataAnnotations/PlutoContext.cs
+++ Section5.Overriding-Code-First/DataAnnotations/DataAnnotations/PlutoContext.cs
@@ -4,13 +4,25 @@ namespace DataAnnotations
 {
     public class PlutoContext : DbContext
     {
+        /// <summary>
+        /// Create a new instance of PlutoContext. 
+        /// </summary>
         public PlutoContext()
             : base("name=PlutoContext")
         {
         }
 
+        /// <summary>
+        /// Table Authors in the Database Pluto_FluentAPI.
+        /// </summary>
         public virtual DbSet<Author> Authors { get; set; }
+        /// <summary>
+        /// Table Courses in the Database Pluto_FluentAPI.
+        /// </summary>
         public virtual DbSet<Course> Courses { get; set; }
+        /// <summary>
+        /// Table Tags in the Database Pluto_FluentAPI.
+        /// </summary>
         public virtual DbSet<Tag> Tags { get; set; }
     }
 }
