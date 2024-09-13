93fcd00473f9fff9b1174f349358066aef613e76
Stanislav Ivanov <40460292+staivanov@users.noreply.github.com>
2024-09-13 12:10:28 +0300

    Code was documented.

--- Section5.Overriding-Code-First/Section5.OverridingCodeFirst/Courses.cs
+++ Section5.Overriding-Code-First/Section5.OverridingCodeFirst/Courses.cs
@@ -7,26 +7,49 @@ namespace Section4.OverridingCodeFirst
     [Table("Courses")]
     public partial class Courses
     {
+        /// <summary>
+        /// Create a new instance of Course object with Tags.
+        /// </summary>
         public Courses()
         {
             Tags = new HashSet<Tags>();
         }
-
+        /// <summary>
+        /// Gets or sets the id of the course. 
+        /// </summary>
         public int Id { get; set; }
 
         public string Title { get; set; }
 
+        /// <summary>
+        /// Gets or sets the description of the course.
+        /// </summary>
         [Required]
         public string Description { get; set; }
 
+
+        /// <summary>
+        /// Gets or sets the level of the course.
+        /// </summary>
         public int Level { get; set; }
 
+        /// <summary>
+        /// Gets or sets the fullprice of the course.
+        /// </summary>
         public float FullPrice { get; set; }
 
+        /// <summary>
+        /// Gets or sets the author foreign key.
+        /// </summary>
         public int? Author_Id { get; set; }
-
+        /// <summary>
+        /// Gets or sets the author of the course.
+        /// </summary>
         public virtual Authors Authors { get; set; }
 
+        /// <summary>
+        /// Gets or sets the tags of the course.
+        /// </summary>
         public virtual ICollection<Tags> Tags { get; set; }
     }
 }
