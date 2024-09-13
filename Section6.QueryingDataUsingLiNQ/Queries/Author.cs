7e9409302ac0c978641dde7300ed27fab0ecace1
Stanislav Ivanov <40460292+staivanov@users.noreply.github.com>
2024-09-13 17:19:20 +0300

    LiNQ extensions and syntax.

--- Section6.QueryingDataUsingLiNQ/Queries/Author.cs
+++ Section6.QueryingDataUsingLiNQ/Queries/Author.cs
@@ -4,15 +4,27 @@ namespace Queries
 {
     public class Author
     {
+        /// <summary>
+        /// Create a new instance of Author object with Courses.
+        /// </summary>
         public Author()
         {
             Courses = new HashSet<Course>();
         }
 
+        /// <summary>
+        /// Gets or sets the id of the author.
+        /// </summary>
         public int Id { get; set; }
 
+        /// <summary>
+        /// Gets or sets the name of the author.
+        /// </summary>
         public string Name { get; set; }
 
+        /// <summary>
+        /// Gets or sets the courses of the author.
+        /// </summary>
         public virtual ICollection<Course> Courses { get; set; }
     }
 }
--- Section6.QueryingDataUsingLiNQ/Queries/Program.cs
+++ Section6.QueryingDataUsingLiNQ/Queries/Program.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 using static System.Console;
 
 namespace Queries
@@ -11,28 +10,69 @@ namespace Queries
         public static void Main()
         {
             string keyword = "c#";
+            int courseLevel = 1;
 
-            IOrderedQueryable<Course> cSharpCourses =
+            //Restriction
+            IQueryable<Course> coursesWithLevel =
                 from c in _context.Courses
-                where c.Name.Contains(keyword)
-                orderby c.Name
+                where c.Level == courseLevel
                 select c;
 
-            foreach (Course course in cSharpCourses)
+            //Ordering
+            IOrderedQueryable<Course> orderedCourses =
+                from c in _context.Courses
+                where c.Level == courseLevel
+                orderby c.Level ascending
+                select c;
+
+            //Grouping
+            IQueryable<IGrouping<int, Course>> groupedCourses =
+                from c in _context.Courses
+                group c by c.Level
+                into g
+                select g;
+
+            //Joins
+            var query =
+                from c in _context.Courses
+                join a in _context.Authors on c.AuthorId equals a.Id
+                select new
+                {
+                    CourseName = c.Name,
+                    AuthorName = a.Name
+                };
+
+            //Restriction plus ordering
+            IQueryable<Course> coursesWithLevelv2 = _context.Courses
+                .Where(c => c.Level == courseLevel)
+                .OrderBy(o => o.Level);
+
+            //Projection
+            IQueryable<Tag> tags = _context.Courses
+                .Where(c => c.Level == courseLevel)
+                .OrderByDescending(c => c.Name)
+                .ThenByDescending(c => c.Level)
+                .SelectMany(c => c.Tags);
+
+            //Set operations
+            IQueryable<Tag> tagsV2 = _context.Courses
+                .Where(c => c.Level == courseLevel)
+                .OrderByDescending(c => c.Name)
+                .ThenByDescending(c => c.Level)
+                .SelectMany(c => c.Tags)
+                .Distinct();
+
+            IQueryable<IGrouping<int, Course>> groups = _context.Courses
+                    .GroupBy(c => c.Level);
+
+            foreach (var group in groups)
             {
-                WriteLine(course.Name);
-            }
+                WriteLine("Key: " + group.Key);
 
-            IOrderedQueryable<Course> cSharpCoursesV2 = _context.Courses
-                .Where(c => c.Name.Contains(keyword))
-                .OrderBy(c => c.Name);
-
-
-            WriteLine("Version 2 data that was quered.");
-
-            foreach (Course course in cSharpCoursesV2)
-            {
-                WriteLine(course.Name);
+                foreach (var course in group)
+                {
+                    WriteLine("\t" + course.Name);
+                }
             }
         }
     }
