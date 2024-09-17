fadf0ad1ce7a0e6fb901983b59e0ed8da0ef8e77
Stanislav Ivanov <40460292+staivanov@users.noreply.github.com>
2024-09-17 10:08:14 +0300

    Adding a new object in db.

---
+++ Section8.UpdatingData/UpdatingData/UpdatingData/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace UpdatingData
+{
+    public class Program
+    {
+        private static readonly PlutoEntities _context = new PlutoEntities();
+
+        public static void Main()
+        {
+            //ChangeTracker see this as a new existing object.
+            Course course = new Course()
+            {
+                Title = "Python",
+                Description = "This is deep course about Python language.",
+                FullPrice = 19,
+                Level = 1,
+                Author = new Author()
+                {
+                    AuthorID = 5,
+                    Name = "Svetlin Nakov"
+                }
+            };
+            _context.Courses.Add(course);
+            _context.SaveChanges();
+        }
+    }
+}
