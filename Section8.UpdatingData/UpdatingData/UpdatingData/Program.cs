19811c8c3c7ace21a5cf17baf11fc06e9b8dd7ec
Stanislav Ivanov <40460292+staivanov@users.noreply.github.com>
2024-09-17 10:15:40 +0300

    Removing an object.

--- Section8.UpdatingData/UpdatingData/UpdatingData/Program.cs
+++ Section8.UpdatingData/UpdatingData/UpdatingData/Program.cs
@@ -6,20 +6,8 @@
 
         public static void Main()
         {
-            //ChangeTracker see this as a new existing object.
-            Course course = new Course()
-            {
-                Title = "Python",
-                Description = "This is deep course about Python language.",
-                FullPrice = 19,
-                Level = 1,
-                Author = new Author()
-                {
-                    AuthorID = 5,
-                    Name = "Svetlin Nakov"
-                }
-            };
-            _context.Courses.Add(course);
+            Author author = _context.Authors.Find(3);
+            _context.Authors.Remove(author);
             _context.SaveChanges();
         }
     }
